183b04bcd40c890c50a0caa0da42b0d64e1ce836
Ashley-Mălina Hurrelbrink <ahurrelbrink98@yahoo.com>
2026-05-22 14:16:56 +0300

    Revert "feat: setup github pages"
    This reverts commit a33e3bc8a2f9b668e3a3bab4e6ff7782cd6fbfed.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
 
 export default defineConfig({
-  base: '/vue-map-project/',
   plugins: [createVuePlugin()],
 })
